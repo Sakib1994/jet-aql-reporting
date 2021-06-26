9968e61e429585452dac714bcfe4cbdec796ae85
Sakib1994 <mdnazmussakibcse@gmail.com>
2021-06-26 07:13:27 +0600

    change in google hourly fetch

--- app/Console/Commands/fetchHourlyGoogleApi.php
+++ app/Console/Commands/fetchHourlyGoogleApi.php
@@ -65,70 +65,62 @@ class fetchHourlyGoogleApi extends Command
     public function fetchFromApi(GoogleAdsClient $googleAdsClient, int $customerId, int $accountId)
     {
         printf($customerId . " " . $accountId . "\n");
-        $getTheNow = Carbon::now('Asia/Tokyo');
+        $getTheNow = Carbon::now('+8:30');
+        dump("Current time: ".$getTheNow->isoFormat('YYYY-MM-DD HH:mm:ss'));
         $date = $getTheNow->isoFormat('YYYY-MM-DD');
+        $hournow = $getTheNow->isoFormat('YYYY-MM-DD HH:00:00');
         $hourtoSave = $getTheNow->sub(1, 'hour')->isoFormat('HH');
-        $hour = $getTheNow->sub(1, 'hour')->isoFormat('HH');
-        /*$date = "2021-06-21";
-        // $hourlower = $getTheNow->sub(2,'hour')->isoFormat('HH');
-        for ($hour = 0; $hour < 24; $hour++) {
-            $hourProcess = $hour < 10 ? "0$hour" : "$hour";
-            $hourtoSave = $hour < 9 ? "0" . ($hour + 1) : "" . ($hour + 1);*/
-            // $time = "$date $hourtosave:00:00";
-            $time = "$date $hourtoSave:00:00";
-            print_r("$customerId $time\n");
-            // データがデータベースにすでに存在する場合は、データを確認する。
-            // 存在する場合は、終了します。
-            if ($hourtoSave!="24") {
-                $isSaved = HourlyAdsData::where('time', $time)
+        print_r("Customer ID: $customerId Time: $hournow\n");
+        // データがデータベースにすでに存在する場合は、データを確認する。
+        // 存在する場合は、終了します。
+        if ($hourtoSave != "24") {
+            $isSaved = HourlyAdsData::where('time', 'like', $hournow)
                 ->firstWhere('AdsAccountId', $accountId);
+        }
+        if ($isSaved) {
+            return 0;
+            // $this->error('Already exist in the databsse or no data from api.');
+            // continue;
+        }
+        // 毎時データグーグルを取得します。
+        $googleAdsServiceClient = $googleAdsClient->getGoogleAdsServiceClient();
+        // これは特別なクエリ言語ですGAQL.dataはこれを使用してグーグルからフェッチされます。
+        $queryFromBuider = "SELECT customer.descriptive_name, segments.hour, metrics.clicks, metrics.ctr, metrics.impressions, metrics.conversions, metrics.average_cpc, metrics.cost_per_conversion, metrics.cost_micros, metrics.conversions_from_interactions_rate FROM customer WHERE segments.date = '$date' AND segments.hour = $hourtoSave";
+        $response = $googleAdsServiceClient->search($customerId, $queryFromBuider, ['pageSize' => self::PAGE_SIZE]);
+        // APIからデータをループして、データを取得します。
+        foreach ($response->iterateAllElements() as $googleAdsRow) {
+            if ($googleAdsRow->getMetrics()->getClicks() == 0) {
+                continue;
             }
-            if ($isSaved) {
-                // $this->error('Already exist in the databsse or no data from api.');
-                return 0;
-                // continue;
-            }
-            // 毎時データグーグルを取得します。
-            $googleAdsServiceClient = $googleAdsClient->getGoogleAdsServiceClient();
-            // これは特別なクエリ言語ですGAQL.dataはこれを使用してグーグルからフェッチされます。
-            $queryFromBuider = "SELECT customer.descriptive_name, segments.hour, metrics.clicks, metrics.ctr, metrics.impressions, metrics.conversions, metrics.average_cpc, metrics.cost_per_conversion, metrics.cost_micros, metrics.conversions_from_interactions_rate FROM customer WHERE segments.date = '$date' AND segments.hour = $hour";
-            $response = $googleAdsServiceClient->search($customerId, $queryFromBuider, ['pageSize' => self::PAGE_SIZE]);
-            // APIからデータをループして、データを取得します。
-            foreach ($response->iterateAllElements() as $googleAdsRow) {
-                if ($googleAdsRow->getMetrics()->getClicks() == 0) {
-                    // return 0;
-                    continue;
-                }
-                printf(
-                    "Customer '%s', hour %s,  Clicks %d, Impressions %d, ctr %.2f%%, Cost %.2f, CPC %4.2f, Conversions %d conversion_rate %.2f and CostPerConversion %.2f\n",
-                    $googleAdsRow->getCustomer()->getDescriptiveName(),
-                    $googleAdsRow->getSegments()->getHour(),
-                    $googleAdsRow->getMetrics()->getClicks(),
-                    $googleAdsRow->getMetrics()->getImpressions(),
-                    round($googleAdsRow->getMetrics()->getCtr() * 100, 2),
-                    round($googleAdsRow->getMetrics()->getCostMicros() / 1000000),
-                    round($googleAdsRow->getMetrics()->getAverageCpc() / 1000000),
-                    $googleAdsRow->getMetrics()->getConversions(),
-                    round($googleAdsRow->getMetrics()->getConversionsFromInteractionsRate() * 100, 2),
-                    round($googleAdsRow->getMetrics()->getCostPerConversion() / 1000000),
-                    PHP_EOL
-                );
-                // グーグルから毎時データを取得し、データベースに保存します。
-                $hourlyAdsData = new HourlyAdsData([
-                    "AdsAccountId" => $accountId,
-                    "time" => $time,
-                    "clicks" => $googleAdsRow->getMetrics()->getClicks(),
-                    "impressions" => $googleAdsRow->getMetrics()->getImpressions(),
-                    "ctr" => round($googleAdsRow->getMetrics()->getCtr() * 100, 2),
-                    "cost" => round($googleAdsRow->getMetrics()->getCostMicros() / 1000000),
-                    "cpc" => round($googleAdsRow->getMetrics()->getAverageCpc() / 1000000),
-                    "conversions" => $googleAdsRow->getMetrics()->getConversions(),
-                    "conversions_rate" => round($googleAdsRow->getMetrics()->getConversionsFromInteractionsRate() * 100, 2),
-                    "cost_per_conversion" => round($googleAdsRow->getMetrics()->getCostPerConversion() / 1000000),
-                ]);
-                $hourlyAdsData->save();
-            }
-        // }
+            printf(
+                "Customer '%s', hour %s,  Clicks %d, Impressions %d, ctr %.2f%%, Cost %.2f, CPC %4.2f, Conversions %d conversion_rate %.2f and CostPerConversion %.2f\n",
+                $googleAdsRow->getCustomer()->getDescriptiveName(),
+                $googleAdsRow->getSegments()->getHour(),
+                $googleAdsRow->getMetrics()->getClicks(),
+                $googleAdsRow->getMetrics()->getImpressions(),
+                round($googleAdsRow->getMetrics()->getCtr() * 100, 2),
+                round($googleAdsRow->getMetrics()->getCostMicros() / 1000000),
+                round($googleAdsRow->getMetrics()->getAverageCpc() / 1000000),
+                $googleAdsRow->getMetrics()->getConversions(),
+                round($googleAdsRow->getMetrics()->getConversionsFromInteractionsRate() * 100, 2),
+                round($googleAdsRow->getMetrics()->getCostPerConversion() / 1000000),
+                PHP_EOL
+            );
+            // グーグルから毎時データを取得し、データベースに保存します。
+            $hourlyAdsData = new HourlyAdsData([
+                "AdsAccountId" => $accountId,
+                "time" => $hournow,
+                "clicks" => $googleAdsRow->getMetrics()->getClicks(),
+                "impressions" => $googleAdsRow->getMetrics()->getImpressions(),
+                "ctr" => round($googleAdsRow->getMetrics()->getCtr() * 100, 2),
+                "cost" => round($googleAdsRow->getMetrics()->getCostMicros() / 1000000),
+                "cpc" => round($googleAdsRow->getMetrics()->getAverageCpc() / 1000000),
+                "conversions" => $googleAdsRow->getMetrics()->getConversions(),
+                "conversions_rate" => round($googleAdsRow->getMetrics()->getConversionsFromInteractionsRate() * 100, 2),
+                "cost_per_conversion" => round($googleAdsRow->getMetrics()->getCostPerConversion() / 1000000),
+            ]);
+            $hourlyAdsData->save();
+        }
         return 1;
     }
 }
